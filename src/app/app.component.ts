1711fedbdd5240b785479efb33065d8893ca11d5
Alex Herrera <herrera.luis@uabc.edu.mx>
2018-01-04 02:41:48 -0800

    Complete prototype

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import { Store } from '@ngrx/store';
+import * as fromAuth from '../pages/auth/reducers';
+import { SigninPageComponent } from '../pages/auth/containers/signin-page/signin-page.component';
+import { SignupPageComponent } from '../pages/auth/containers/signup-page/signup-page.component';
+import { ComposeEmailPageComponent } from '../pages/emails/containers/compose-email-page/compose-email-page.component';
+import { EmailsPageComponent } from '../pages/emails/containers/emails-page/emails-page.component';
 
 @Component({
   templateUrl: 'app.html'
@@ -12,18 +16,38 @@ import { ListPage } from '../pages/list/list';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = HomePage;
+  rootPage: any = SigninPageComponent;
 
-  pages: Array<{title: string, component: any}>;
+  pages: Array<{ icon: string, title: string, component: any }>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  constructor(public platform: Platform,
+    public statusBar: StatusBar,
+    public splashScreen: SplashScreen,
+    private store: Store<fromAuth.State>
+  ) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { icon: 'log-in', title: 'Sign In', component: SigninPageComponent },
+      { icon: 'person', title: 'Sign Up', component: SignupPageComponent }
     ];
+
+    this.store.select(fromAuth.getSignedIn)
+      .subscribe((signedIn) => {
+        if (signedIn) {
+          this.pages = [
+            { icon: 'archive', title: 'Inbox', component: EmailsPageComponent },
+            { icon: 'create', title: 'Compose', component: ComposeEmailPageComponent },
+            { icon: 'log-out', title: 'Sign Out', component: SigninPageComponent },
+          ];
+        } else {
+          this.pages = [
+            { icon: 'log-in', title: 'Sign In', component: SigninPageComponent },
+            { icon: 'person', title: 'Sign Up', component: SignupPageComponent }
+          ];
+        }
+      })
 
   }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,28 @@ import { ListPage } from '../pages/list/list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+
+import { metaReducers } from './reducers';
+import { AuthModule } from '../pages/auth/auth.module';
+import { EmailsModule } from '../pages/emails/emails.module';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyB24Lt2boVoqEvQgBUMD6CYMHfn5IkSndg",
+  authDomain: "email-app-667ce.firebaseapp.com",
+  databaseURL: "https://email-app-667ce.firebaseio.com",
+  projectId: "email-app-667ce",
+  storageBucket: "email-app-667ce.appspot.com",
+  messagingSenderId: "386335910406"
+};
+
 @NgModule({
   declarations: [
     MyApp,
@@ -18,6 +40,43 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    /**
+     * StoreModule.forRoot is imported once in the root module, accepting a reducer
+     * function or object map of reducer functions. If passed an object of
+     * reducers, combineReducers will be run creating your application
+     * meta-reducer. This returns all providers for an @ngrx/store
+     * based application.
+     */
+    StoreModule.forRoot({}, { metaReducers }),
+
+    /**
+     * Store devtools instrument the store retaining past versions of state
+     * and recalculating new states. This enables powerful time-travel
+     * debugging.
+     *
+     * To use the debugger, install the Redux Devtools extension for either
+     * Chrome or Firefox
+     *
+     * See: https://github.com/zalmoxisus/redux-devtools-extension
+     */
+    StoreDevtoolsModule.instrument(),
+
+    /**
+     * EffectsModule.forRoot() is imported once in the root module and
+     * sets up the effects class to be initialized immediately when the
+     * application starts.
+     *
+     * See: https://github.com/ngrx/platform/blob/master/docs/effects/api.md#forroot
+     */
+    EffectsModule.forRoot([]),
+
+    AuthModule.forRoot(),
+
+    EmailsModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
